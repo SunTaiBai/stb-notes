4135016173ad26595b82a7496bb6a459c178199f
SunTaiBai <38248441+SunTaiBai@users.noreply.github.com>
2023-05-18 07:07:48 +0800

    添加评论

--- docs/.vitepress/theme/components/git-talk.vue
+++ docs/.vitepress/theme/components/git-talk.vue
@@ -2,19 +2,42 @@
 import md5 from 'md5'
 import Gitalk from 'gitalk'
 import 'gitalk/dist/gitalk.css'
-import { ref, unref } from 'vue'
+import { onMounted } from 'vue'
+import { message } from 'ant-design-vue'
 
-const commentConfig = ref({
-  clientID: '',
-  clientSecret: '',
-  repo: '你的仓库名',
-  owner: '你的账户名',
-  admin: ['你的账户名'],
+const gitalk = new Gitalk({
+  clientID: 'f8b4f7d249504adb975c',
+  clientSecret: '137a4407c3c48fcb2ff416efc36632f6366ff725',
+  repo: 'DocComment',
+  owner: 'SunTaiBai',
+  admin: ['SunTaiBai'],
   id: md5(location.pathname),
   distractionFreeMode: false,
 })
-const gitalk = new Gitalk(unref(commentConfig))
-gitalk.render('gitalk-container')
+// 异步获取点赞元素
+function getLikeElementAsync(className: string): Promise<HTMLCollectionOf<Element>> {
+  return new Promise((resolve) => {
+    // 定义定时器检查点赞元素是否已加载
+    const intervalId = setInterval(() => {
+      const likeElem = document.getElementById('gitalk-container')?.getElementsByClassName(className) as HTMLCollectionOf<Element>
+      if (likeElem.length > 0) {
+        clearInterval(intervalId)
+        resolve(likeElem)
+      }
+    }, 100)
+  })
+}
+onMounted(async () => {
+  gitalk.render('gitalk-container')
+  const gtCommentLikeList = await getLikeElementAsync('gt-comment-like')
+  for (let i = 0; i < gtCommentLikeList?.length; i++) {
+    gtCommentLikeList[i].addEventListener('click', () => {
+      // 如果点赞，先判断有没有登录
+      if (!window.localStorage.getItem('GT_ACCESS_TOKEN'))
+        message.error('点赞前，请您先进行登录')
+    })
+  }
+})
 </script>
 
 <template>
